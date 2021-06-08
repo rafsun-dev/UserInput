64e4e4ba2342f17b6389081bf74fb531c7b9525b
rafsun <rafsun.dev@gmail.com>
2021-06-08 20:26:45 +0600

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter a number: ");
+//        int number = input.nextInt();
+//        System.out.println(number);
+
+        Scanner input2 = new Scanner(System.in);
+        System.out.print("Enter a name: ");
+        String name = input2.nextLine();
+        System.out.println(name);
+    }
+}
